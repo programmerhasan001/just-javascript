b7c3b4443a4dd85a54faf74d11182ee810595139
Md Hasan <86654719+programmerhasan001@users.noreply.github.com>
2023-12-11 16:41:51 +0000

    data_types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version of js
+
+// alert(3 + 3); // we are using node js not browser
+
+console.log(3 + 3);
+
+let name = "hasan"
+let age = 18
+let isLoggedIn = true
+let state;
+let phoneNo = null
+
+// dataTypes of js
+// Number
+// BigInt
+// String
+// Boolean
+// null => it self a value and it's type is object
+// undefined => it's type is undefined and it assigned a value of undefined
+// symbol // for unique
+// object
+
+console.log(typeof age)
+console.log(typeof null)
