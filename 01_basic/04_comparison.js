4cb16d27f6e2a2108d5247eb30895a0c6120b4aa
Md Hasan <86654719+programmerhasan001@users.noreply.github.com>
2024-01-26 15:06:11 +0000

    comparison with and without data type

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,14 @@
+console.log("2" > 1);
+console.log("02" > 1);
+console.log("02");
+console.log(Number("02"));
+
+console.log(null > 0);  // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+
+console.log(undefined > 0); // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+
