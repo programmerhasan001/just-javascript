4fe74eaaf92082264be4abd5724c365ddefe0cf0
Md Hasan <86654719+programmerhasan001@users.noreply.github.com>
2024-01-26 15:06:42 +0000

    comparison with and without data type

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = null
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);
-console.log(valueInNumber >= 0 && score !== null);
+// console.log(valueInNumber);
+// console.log(valueInNumber >= 0 && score !== null);
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,8 +13,29 @@ console.log(valueInNumber >= 0 && score !== null);
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn, "Boolean");
+// console.log(booleanIsLoggedIn, "Boolean");
 
 // Boolean()
 // 1 => true; 0 => false;
 // "" => false; "adjk" => true;
+
+/* ========== Operations ====================*/
+const value = 3;
+const negValue = -value;
+
+let x = 3;
+let y = x++;
+
+console.log(x, y)
+
+let a = 2;
+let b = ++a;
+
+console.log(a, b)
+
+const output = Number(true) // 1
+const output1 = Number("") // 0
+console.log(+true) // 1
+console.log(+"") // 0
+
+console.log(negValue)
